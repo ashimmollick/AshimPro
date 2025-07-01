e44f39577bed3ca9448430c6808b8619d7ef5e59
Ashim-mollick <ashimmollick46@gmail.com>
2025-07-01 23:39:53 +0600

    skills section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-
 import AboutMeMain from "./Components/AboutMeSection/AboutMeMain";
+import HealperSection from "./Components/HealperSection";
 import HeroGradient from "./Components/heroSections/HeroGradient";
 import HeroMain from "./Components/heroSections/HeroMain";
 import SubHeroSections from "./Components/heroSections/SubHeroSections";
 import NavbarMain from "./Components/navbar/NavbarMain";
+import SkillsMain from "./Components/SkillsSection/SkillsMain";
 
 function App() {
   return (
     <main className="font-body">
       <NavbarMain />
       <HeroMain />
-      <HeroGradient/>
-      <SubHeroSections/>
-      <AboutMeMain/>
+      <HeroGradient />
+      <SubHeroSections />
+      <AboutMeMain />
+      <SkillsMain />
+      <HealperSection />
     </main>
   );
 }
---
+++ src/Components/SkillsSection/SubSkills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SubSkills = () => {
+    return (
+        <div>
+            <div></div>
+            <img src="../../../public/images/subSkills.jpg" alt="Sub Skills" />
+        </div>
+    );
+};
+
+export default SubSkills;
