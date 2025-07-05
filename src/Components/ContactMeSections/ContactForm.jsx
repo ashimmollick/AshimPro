a010ad1f6e1a7862b85a713963123cbb54e30134
Ashim-mollick <ashimmollick46@gmail.com>
2025-07-05 23:52:17 +0600

    Contact form

--- src/Components/ContactMeSections/ContactForm.jsx
+++ src/Components/ContactMeSections/ContactForm.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const ContactForm = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Your Name"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <input
+          type="email"
+          placeholder="Your Email"
+          required
+          className="h-12 rounded-lg bg-lightBrown px-2"
+        />
+        <textarea
+          type="text"
+          placeholder="Message"
+          required
+          rows="9"
+          cols="50"
+          className=' rounded-lg bg-lightBrown p-2'
+        />
+        <button type="submit" className="w-full rounded-lg border border-cyan text-white h-12 font-bold text-xl hover:bg-darkCyan bg-cyan transition-all duration-500 ">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
