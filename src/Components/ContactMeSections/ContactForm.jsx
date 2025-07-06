3661dfbe4eb74b2854d22d5b219926c9fc53ac06
Ashim-mollick <ashimmollick46@gmail.com>
2025-07-06 12:08:44 +0600

    contact from Email Join

--- src/Components/ContactMeSections/ContactForm.jsx
+++ src/Components/ContactMeSections/ContactForm.jsx
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+  const form=useRef();
+  const [name,setName]=useState('');
+  const [email,setEmail]=useState('');
+  const [message,setMessage]=useState('');
+  const [success,setSuccess]=useState('');
+  const handleName=(e)=>{
+    setName(e.target.value);
+  } 
+  const handleEmail=(e)=>{
+    setEmail(e.target.value);
+  } 
+  const handleMessage=(e)=>{
+    setMessage(e.target.value);
+  }
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_fmbf9sa', 'template_n7u6p8c', form.current, {
+        publicKey: 'F18KRxj4St5uDbVSg',
+      })
+      .then(
+        () => {
+          setName('');
+          setEmail('');
+          setMessage('');
+          setSuccess('Message Send')
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <div>
-      <form action="" className="flex flex-col gap-4">
+      <p className="text-cyan">{success}</p>
+      <form action="" className="flex flex-col gap-4 text-white" ref={form} onSubmit={sendEmail}>
         <input
+        name="from_name"
           type="text"
           placeholder="Your Name"
           required
           className="h-12 rounded-lg bg-lightBrown px-2"
+          value={name}
+          onChange={handleName}
         />
         <input
+        name="from_email"
           type="email"
           placeholder="Your Email"
           required
           className="h-12 rounded-lg bg-lightBrown px-2"
+          value={email}
+          onChange={handleEmail}
         />
         <textarea
+        name="message"
           type="text"
           placeholder="Message"
           required
           rows="9"
           cols="50"
           className=' rounded-lg bg-lightBrown p-2'
+          value={message}
+          onChange={handleMessage}
         />
         <button type="submit" className="w-full rounded-lg border border-cyan text-white h-12 font-bold text-xl hover:bg-darkCyan bg-cyan transition-all duration-500 ">Send</button>
       </form>
